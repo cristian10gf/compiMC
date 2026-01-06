d0004643041e08e1c24b5dcf183e85f7bb1ee0ea
cristian10gf <cristiandavidgf@gmail.com>
2026-01-05 20:45:08 -0500

    feat: eliminar referencia al componente HistoryPanel y actualizar exportaciones

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -4,7 +4,6 @@
 
 export { MainSidebar } from './main-sidebar';
 export { HeroSection } from './hero-section';
-export { HistoryPanel } from './history-panel';
 export { HistoryPanel as HistoryPanelV2 } from './history-panel-v2';
 export { Footer } from './footer';
 export { AppLayout } from './app-layout';
