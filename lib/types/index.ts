c0cb19f3ac7ee3eb01edee576303a40620807fe4
cristian10gf <cristiandavidgf@gmail.com>
2026-01-05 22:20:01 -0500

    feat: eliminar tipos de visualización de React Flow del módulo de tipos

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -6,6 +6,5 @@ export * from './automata';
 export * from './grammar';
 export * from './syntax-analysis';
 export * from './token';
-export * from './graph';
 export * from './analysis';
 export * from './history';
